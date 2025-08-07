54921216a64e7582b673fd5ae7c54b09248cdf1f
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-08-07 13:56:51 +0530

    update

--- src/main/java/lk/ijse/gdse/springdeployment/controller/JobController.java
+++ src/main/java/lk/ijse/gdse/springdeployment/controller/JobController.java
@@ -12,9 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/job")
+@RequestMapping("/api/v1/job")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:63343")
 public class JobController {
 
     private final JobService jobService;
